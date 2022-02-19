2553af846e2d7e2af1f4c32723d7c53a797e074f
Brock Herion <brockheriondev@gmail.com>
2022-02-19 17:23:42 -0600

    Added validation for input in list

--- src/wordle/gameplay.rs
+++ src/wordle/gameplay.rs
@@ -1,7 +1,8 @@
+use crate::wordle::words;
 use colored::*;
 use std::io::stdin;
 
-pub fn get_player_guess(word_length: usize) -> Result<String, String> {
+pub fn get_and_validate_player_guess(word_length: usize) -> Result<String, String> {
 	println!("Please enter a {} letter word: ", word_length);
 
 	let mut player_guess = String::new();
@@ -19,7 +20,9 @@ fn validate_player_guess(guess: String, word_length: usize) -> Result<String, St
 		)
 		.red()
 		.to_string());
-	} else if guess.len() < word_length {
+	}
+
+	if guess.len() < word_length {
 		return Err(format!(
 			"\nYour guess was too short! Guesses must be {} letters long\n",
 			word_length
@@ -28,5 +31,19 @@ fn validate_player_guess(guess: String, word_length: usize) -> Result<String, St
 		.to_string());
 	}
 
+	if !validate_word_in_list(&guess) {
+		return Err("\nYour guess was not in the list!\n".red().to_string());
+	}
+
 	Ok(guess)
+}
+
+fn validate_word_in_list(guess: &String) -> bool {
+	let found = words::find_word(guess);
+
+	if found == None {
+		return false;
+	}
+
+	true
 }
